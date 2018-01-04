1052fdd92e7adac335f6b0dab38063f179c26124
afonin dmitry <dmitriy.afonin@walletone.com>
2018-01-04 22:24:50 +0500

    added an extension to IWebHost

---
+++ src/AfoninDmitry.AspNetCore.Hosting.WindowsServices/DummyService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using DasMulli.Win32.ServiceUtils;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AfoninDmitry.AspNetCore.Hosting.WindowsServices
+{
+    internal class DummyService : IWin32Service
+    {
+        private readonly IWebHost webHost;
+        public bool stopRequestedByWindows { get; private set; }
+        public string ServiceName { get; }
+
+        public DummyService(IWebHost webHost, string serviceName)
+        {
+            this.ServiceName = serviceName;
+            this.webHost = webHost;
+        }
+
+
+        public void Start(string[] startupArguments, ServiceStoppedCallback serviceStoppedCallback)
+        {
+            webHost
+                .Services
+                .GetRequiredService<IApplicationLifetime>()
+                .ApplicationStopped
+                .Register(() =>
+                {
+                    if (stopRequestedByWindows == false)
+                    {
+                        serviceStoppedCallback();
+                    }
+                });
+
+            webHost.Start();
+        }
+
+        public void Stop()
+        {
+            stopRequestedByWindows = true;
+            webHost.Dispose();
+        }
+    }
+}
